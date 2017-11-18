8f4acf5cf11608608c766623c1332e934eae5022
dewararajkiran <dewara.rajkiran@gmail.com>
2017-11-18 14:57:02 +0530

    Update database.js

--- config/database.js
+++ config/database.js
@@ -1,4 +1,4 @@
 module.exports = {
-    remoteUrl : 'mongodb://node:nodeuser@mongo.onmodulus.net:27017/uwO3mypu',
-    localUrl: 'mongodb://localhost/meanstacktutorials'
+    remoteUrl : 'postgres://aammnxmkqracxg:74c0493858a4b0b7a8756d049a9cf5154e0991e753ad5bb1ae07dda8222a9f7f@ec2-50-19-219-69.compute-1.amazonaws.com:5432/d5i0rh888g11ej',
+    localUrl: 'postgres://aammnxmkqracxg:74c0493858a4b0b7a8756d049a9cf5154e0991e753ad5bb1ae07dda8222a9f7f@ec2-50-19-219-69.compute-1.amazonaws.com:5432/d5i0rh888g11ej'
 };
